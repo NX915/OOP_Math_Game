87487b3a6f3eea569e5ecd5b590a6797ddca1c19
Sean Ni <nixiang915@hotmail.com>
2020-10-12 20:16:56 +0000

    add basic framwork

--- math_game.rb
+++ math_game.rb
@@ -19,5 +19,40 @@ create questions and determine if answer is correct
 =end
 
 class MathGame
+  def initialize
+    @player1 = Player.new("Player 1")
+    @player2 = Player.new("Player 2")
+  end
 
+  def play
+    question = Question.new
+
+    puts "Game start!"
+    puts "#{@player1.name}: #{question.question}"
+    answer = gets.chomp
+    
+    puts answer == question.answer
+  end
 end
+
+class Player
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @score = 0
+    @lives = 3
+  end
+end
+
+class Question
+  attr_reader :question, :answer
+
+  def initialize
+    @question = "What is 1 + 1"
+    @answer = "2"
+  end
+end
+
+game = MathGame.new
+game.play
