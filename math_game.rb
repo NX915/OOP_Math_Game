5c147fca928bad8893d32bfc263b98e5a748e573
Sean Ni <nixiang915@hotmail.com>
2020-10-12 19:36:17 +0000

    add inital plan

---
+++ math_game.rb
@@ -0,0 +1,23 @@
+=begin
+
+MathGame
+allow game setup and ending. take in user input and display game interface
+-turn number
+-total players
+-current player
+
+Player
+keep track of players
+-lives
+-score
+
+Question
+create questions and determine if answer is correct
+-question
+-answer
+
+=end
+
+class MathGame
+
+end
