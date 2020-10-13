f4b7f1cf63b3601aee267b5b43ad63ec059f01b6
Sean Ni <nixiang915@hotmail.com>
2020-10-13 01:15:25 +0000

    add basic functionality

--- math_game.rb
+++ math_game.rb
@@ -19,29 +19,71 @@ create questions and determine if answer is correct
 =end
 
 class MathGame
+  attr_accessor :players, :current, :is_over
+  attr_reader :max
+
   def initialize
-    @player1 = Player.new("Player 1")
-    @player2 = Player.new("Player 2")
+    player1 = Player.new("Player 1")
+    player2 = Player.new("Player 2")
+
+    @players = [player1, player2]
+    @current = 0
+    @max = 2
+    @is_over = false
   end
 
   def play
-    question = Question.new
-
     puts "Game start!"
-    puts "#{@player1.name}: #{question.question}"
-    answer = gets.chomp
-    
-    puts answer == question.answer
+
+    while @is_over == false
+      question = Question.new
+
+      puts "#{self.players[self.current].name}: #{question.question}"
+      answer = gets.chomp
+      
+      if (question.is_correct(answer))
+        puts "#{self.players[self.current].name}: YES! You are correct."
+        self.players[self.current].scored
+      else
+        puts "#{self.players[self.current].name}: Seriously? No! It was #{question.answer}"
+        self.players[self.current].lives -= 1
+      end
+
+      if(self.players[self.current].lives == 0)
+        self.is_over = true
+        self.next_player
+        puts "#{self.players[self.current].name} wins with a score of #{self.players[self.current].lives}/3 and answered #{self.players[self.current].score} questions right"
+        puts "----- Game Over -----"
+      else
+        puts "P1: #{self.players[0].lives}/3 vs P2: #{self.players[1].lives}/3"
+        puts "----- NEW TURN -----"
+      end
+
+      self.next_player
+    end
   end
+
+  def next_player
+    self.current += 1
+    if (self.current == self.max)
+      self.current = 0
+    end
+  end
+
 end
 
 class Player
   attr_reader :name
+  attr_accessor :score, :lives
 
   def initialize(name)
     @name = name
     @score = 0
     @lives = 3
+  end
+
+  def scored
+    @score += 1
   end
 end
 
@@ -49,9 +91,19 @@ class Question
   attr_reader :question, :answer
 
   def initialize
-    @question = "What is 1 + 1"
-    @answer = "2"
+    @numbers = [rand(20), rand(20)]
+    @question = "What is #{@numbers[0]} + #{@numbers[1]}"
+    @answer = "#{@numbers[0] + @numbers[1]}"
   end
+
+  def is_correct(answer)
+    if (@answer == answer)
+      return true
+    else
+      return false
+    end
+  end
+
 end
 
 game = MathGame.new
